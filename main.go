7e0a7ca2051c3911feb68a0e6ebb763eac0789d0
was.li <wasuwat.se@gmail.com>
2018-11-07 17:41:08 +0700

    test

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ type ArrayResponse struct {
 
 // Employee struct provides basic employee information
 type Employee struct {
-	gorm.Model
+	ID        int    `gorm:"AUTO_INCREMENT" json:"id"`
 	FirstName string `gorm:"type:varchar(100)" json:"firstname"`
 	LastName  string `gorm:"type:varchar(100)" json:"lastname"`
 	Age       uint   `json:"age"`
